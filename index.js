e19277f53936019a3a6510c749ed71bccad05476
Yeramin Santana <ysantana.developer@gmail.com>
2017-09-04 19:46:27 -0400

    Add break points for headlines strings

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const APP_ID = "amzn1.ask.skill.661b5b23-0ed2-49e6-8d05-23505041fe1f";
 
 const SKILL_NAME = "Dominican News";
 const GET_NEWS_MESSAGE = "Welcome to Dominican News, Here are the latest dominican news: ";
+const END_NEWS_MESSAGE = ".<break time='1s'/> Thanks for staying in synced with Dominican News. Goodbye!"
 const HELP_MESSAGE = "You can say tell me dominican news or, you can say exit... What can I help you with?";
 const HELP_REPROMPT = "What can I help you with?";
 const STOP_MESSAGE = "Goodbye!";
@@ -21,12 +22,12 @@ exports.handler = function(event, context, callback) {
 
 const handlers = {
     'LaunchRequest': function () {
-        this.emit('GetNewsHeadLines');
+        this.emit('GetNewsHeadLinesIntent');
     },
     'GetNewsHeadLinesIntent': function () {
         newsScraper.getNewsHeadlines()
         .then(newsHeadlines => {
-            const speechOutput = GET_NEWS_MESSAGE + newsHeadlines;
+            const speechOutput = GET_NEWS_MESSAGE + newsHeadlines + END_NEWS_MESSAGE;
             this.emit(':tellWithCard', speechOutput, SKILL_NAME, newsHeadlines)
         })
         .catch(err => {
